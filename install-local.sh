0a357c308d4881aea035af00ca326abaef00c222
OpenBioCure Contributor <openbiocure@gmail.com>
2025-06-19 09:19:23 +0400

    fix: small typo

--- install-local.sh
+++ install-local.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Variables
-BASH_LIB_URL="https://raw.githubusercontent.com/mohammadchehab/bash-lib/main/dist/bash-lib.sh"
 BASH_LIB_PATH="/opt/bash-lib/bash-lib.sh"
 SHELL_PROFILE=""
 
@@ -15,8 +14,8 @@ install() {
     # Create the directory if it does not exist
     sudo mkdir -p /opt/bash-lib
 
-    # Download the merged script file
-    if sudo curl -sSL $BASH_LIB_URL -o $BASH_LIB_PATH; then
+    # Copy the local merged script file
+    if sudo cp ./dist/bash-lib.sh $BASH_LIB_PATH; then
         # Make the script executable
         sudo chmod +x $BASH_LIB_PATH
 
@@ -30,9 +29,9 @@ install() {
         source $BASH_LIB_PATH
         export BASH__PATH=/opt/bash-lib
 
-        echo "bash-lib installed successfully. Please restart your terminal or run 'source $SHELL_PROFILE' to apply changes."
+        echo "bash-lib installed successfully from local files. Please restart your terminal or run 'source $SHELL_PROFILE' to apply changes."
     else
-        echo "Failed to download bash-lib.sh. Please check your internet connection and try again."
+        echo "Failed to copy bash-lib.sh. Please check if the file exists in ./dist/"
         exit 1
     fi
 }
@@ -55,4 +54,4 @@ if [ "$1" == "uninstall" ]; then
     uninstall
 else
     install
-fi
+fi 
